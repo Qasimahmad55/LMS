11ac1bc35e56b6b8a310fee355b664030cbc8301
Qasim Ahmad <qasimahmad2555@gmail.com>
2026-04-24 17:14:03 +0500

    feat: set up server with Express, routes, and database connection

---
+++ server/app.ts
@@ -0,0 +1,33 @@
+require('dotenv').config()
+import express, { NextFunction, Request, Response } from 'express'
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
+
+export const app = express()
+
+app.use(express.json({ limit: "50mb" }))
+app.use(cookieParser())
+app.use(cors({
+    origin: process.env.ORIGIN
+}))
+
+//test route
+
+app.get("/test", (req: Request, res: Response, next: NextFunction) => {
+    res.status(200).json({
+        success: true,
+        message: "Api is working"
+    })
+
+})
+//unlnown routes
+
+app.all("/*path", (req: Request, res: Response, next: NextFunction) => {
+    const err = new Error(`Route ${req.originalUrl} not found`) as any
+    err.statusCode = 404
+    next(err)
+
+})
+
+
+
