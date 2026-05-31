753a7dc95062792f861bcced7368e6cad5653e53
Qasim Ahmad <qasimahmad2555@gmail.com>
2026-05-31 23:57:18 +0500

    feat: implement Hero, Header, and ThemeSwitcher components; add navigation items and update global styles

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,23 +1,36 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+// import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Poppins } from "next/font/google";
 import { Josefin_Sans } from "next/font/google";
+import ThemeProvider from "./utils/Theme-provider";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const poppins = Poppins({
   subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-Poppins",
+
+})
+
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-Josefin",
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
 
-export const metadata: Metadata = {
-  title: "ELearning",
-  description: "ELearning is a platform for students to learn and get help from teachers",
-};
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
+
+// export const metadata: Metadata = {
+//   title: "ELearning",
+//   description: "ELearning is a platform for students to learn and get help from teachers",
+// };
 
 export default function RootLayout({
   children,
@@ -25,11 +38,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${poppins.variable} ${josefin.variable} bg-white! bg-no-repeat dark:bg-linear-to-b dark:from-gray-900 dark:to-black duration-300`}>
+        <ThemeProvider attribute='class' defaultTheme="system" enableSystem>
+          {children}
+        </ThemeProvider>
+
+      </body>
     </html>
   );
 }
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,14 +1,36 @@
-import type { Metadata } from "next";
+// import type { Metadata } from "next";
+'use client'
+import { useState } from "react";
+import Heading from "./utils/Heading";
+import Header from "./components/Header";
+import Hero from "./Route/Hero";
 
-export const metadata: Metadata = {
-  title: "ELearning",
-  description:
-    "ELearning is a platform for students to learn and get help from teachers",
-  keywords: ["Programming", "MERN", "AI", "Machine Learning"],
-};
+// export const metadata: Metadata = {
+//   title: "ELearning",
+//   description:
+//     "ELearning is a platform for students to learn and get help from teachers",
+//   keywords: ["Programming", "MERN", "AI", "Machine Learning"],
+// };
 
 const Page = () => {
-  return <div />;
+  const [open, setOpen] = useState(false)
+  const [activeItem, setActiveItem] = useState(0)
+  return (
+    <div>
+      <Heading
+        title="Learning Management System"
+        description=
+        "ELearning is a platform for students to learn and get help from teachers"
+        keywords="Programming, MERN, Redux, ML, AI"
+      />
+      <Header
+        open={open}
+        setOpen={setOpen}
+        activeItem={activeItem}
+      />
+      <Hero/>
+    </div>
+  );
 };
 
 export default Page
